d1d97f578eb4ccb43b9bcea438c894f74880c19c
ClaudiaZaCor <32303175+ClaudiaZaCor@users.noreply.github.com>
2017-10-10 12:20:08 -0500

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,27 @@
 //Pidiendo el numero de tarjeta de credito//
-var numberOfCreditCard = [prompt ("Inserte el numero de su tarjeta.")];
-  numberOfCreditCard = [];
+    var numberOfCreditCard = [prompt ("Inserte el numero de su tarjeta.")];
+    numberOfCreditCard = [];
+
   //definiendo la longitud del array //
     if (numberOfCreditCard.length > 16) {
-        numberOfCreditCard.length = 16;
-    };
+        numberOfCreditCard.length = 16
+        };
+
     // aplicando reverse para elegir los numeros pares desde el final//
     var invertedNumber = numberOfCreditCard.reverse() ;
-    if (invertedNumber [i] == [i] > 1&& [i] % 2 == 0){
-      invertedNumber*2 = resultado1;
-      console.log ("Mostrando los numeros pares");
-    }
-    else {
-      [i] + resultado1 = resultado2;
-    }
-    function isValidCard
-    }if {
-    resultado2 % 10 == 0
-    resultado2 == true /Tarjeta de credito valida/
-    } else {
-    resultado2 == false
-    }
+    var invertedNumber = i
+
+    //Aplicando el algoritmo de Luhn//  
+      var digit = parseInt(invertedNumber.charAt(i), 10);
+        if (digit % 2 == 0 && digit <9 ) {
+            (digit * 2) ;
+            
+      } else {
+          (digit + (digit * 2)) 
+      
+
+      var result = function isValidCard (digit, (digit * 2));
+       return ((digit + (digit * 2)) % 10) == 0;
+      }
+  
+  isValidCard (70)
